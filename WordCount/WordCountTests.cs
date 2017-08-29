20d84aaabde25344cf10056c0b0b47b57cda6b1d
eason.lin62 <eason.lin@titansoft.com.sg>
2017-08-29 23:14:37 +0800

    remove duplicated test.

--- WordCount/WordCountTests.cs
+++ WordCount/WordCountTests.cs
@@ -37,18 +37,6 @@ namespace WordCount
         }
 
         [Test]
-        public void given_a_a_b_should_return_correctly()
-        {
-            CollectionAssert.AreEqual(new Dictionary<string, int> {{"a", 2}, {"b", 1}}, WordCount.Count("a a b"));
-        }
-
-        [Test]
-        public void given_a_b_b_should_return_correctly()
-        {
-            CollectionAssert.AreEqual(new Dictionary<string, int> {{"b", 2}, {"a", 1}}, WordCount.Count("a b b"));
-        }
-
-        [Test]
         public void given_a_b_b_b_c_c_should_return_correctly()
         {
             CollectionAssert.AreEqual(new Dictionary<string, int> {{"b", 3}, {"c", 2}, {"a", 1}},
