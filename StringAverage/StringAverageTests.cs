46a18b1e05efa5eee941b793077814ee622a68dd
eason.lin62 <eason.lin@titansoft.com.sg>
2017-08-30 00:10:07 +0800

    remove duplicated test.

--- StringAverage/StringAverageTests.cs
+++ StringAverage/StringAverageTests.cs
@@ -18,39 +18,9 @@ namespace StringAverage
         }
 
         [Test]
-        public void given_two_should_return_two()
-        {
-            Assert.AreEqual("two", StringAverage.Avg("two"));
-        }
-
-        [Test]
         public void given_zero_should_return_zero()
         {
             Assert.AreEqual("zero", StringAverage.Avg("zero"));
-        }
-
-        [Test]
-        public void given_one_one_should_return_one()
-        {
-            Assert.AreEqual("one", StringAverage.Avg("one one"));
-        }
-
-        [Test]
-        public void given_one_two_should_return_one()
-        {
-            Assert.AreEqual("one", StringAverage.Avg("one two"));
-        }
-
-        [Test]
-        public void given_two_one_should_return_one()
-        {
-            Assert.AreEqual("one", StringAverage.Avg("two one"));
-        }
-
-        [Test]
-        public void given_three_five_should_return_four()
-        {
-            Assert.AreEqual("four", StringAverage.Avg("three five"));
         }
 
         [Test]
