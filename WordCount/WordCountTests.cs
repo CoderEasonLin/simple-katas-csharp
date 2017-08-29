f0d1d46e68f46c257c775fe3224f65f68a68a476
eason.lin62 <eason.lin@titansoft.com.sg>
2017-08-29 23:09:48 +0800

    another test

--- WordCount/WordCountTests.cs
+++ WordCount/WordCountTests.cs
@@ -34,5 +34,12 @@ namespace WordCount
         {
             CollectionAssert.AreEqual(new Dictionary<string, int> {{"a", 1}, {"b", 1}}, WordCount.Count("a b"));
         }
+
+        [Test]
+        public void given_a_b_c_should_return_correctly()
+        {
+            CollectionAssert.AreEqual(new Dictionary<string, int> {{"a", 1}, {"b", 1}, {"c", 1}},
+                WordCount.Count("a b c"));
+        }
     }
 }
