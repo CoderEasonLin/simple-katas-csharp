83bb97ca810f17deb909abe64042e799e743a6ed
eason.lin62 <eason.lin@titansoft.com.sg>
2017-08-29 23:24:56 +0800

    Add files and first failed test.

---
+++ StringAverage/StringAverage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringAverage
+{
+    internal class StringAverage
+    {
+        public static string Avg(string empty)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StringAverage/StringAverageTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace StringAverage
+{
+    [TestFixture]
+    internal class StringAverageTests
+    {
+        [Test]
+        public void string_empty_should_return_na()
+        {
+            Assert.AreEqual("n/a", StringAverage.Avg(""));
+        }
+    }
+}
