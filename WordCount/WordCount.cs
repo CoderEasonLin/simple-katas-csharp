37ed3f3a89fba165517cc7565abe8d9ed054ac6d
eason.lin62 <eason.lin@titansoft.com.sg>
2017-08-29 22:07:30 +0800

    Add first test.

---
+++ WordCount/WordCount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    class WordCount
+    {
+        public static int Count(string empty)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WordCount/WordCountTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WordCount
+{
+    [TestFixture]
+    class WordCountTests
+    {
+        [Test]
+        public void string_empty_should_return_0()
+        {
+            Assert.AreEqual(0, WordCount.Count(""));
+        }
+    }
+}
